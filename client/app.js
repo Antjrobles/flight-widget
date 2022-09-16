86d4f4b91c78ac9eaa8c8ee03671d2921af6b6fc
Antonio Robles <antjrobles@gmail.com>
2022-09-16 22:05:10 +0200

    First commit

---
+++ client/app.js
@@ -0,0 +1,39 @@
+/* const { response } = require("express") */
+
+const tableBody = document.getElementById('table-body')
+
+const getFlight = () => {
+    fetch('http://192.168.0.50:8046/flights')
+        .then(response => response.json())
+        .then(flights =>  {
+        populateTable(flights)
+        })
+        .catch(err => console.log(err))
+}
+
+getFlight()
+
+const populateTable = (flights) => {
+    console.log(flights)
+    for (const flight of flights) {
+    const tableRow = document.createElement('tr')
+    const tableIcon = document.createElement('td')
+    tableIcon.textContent = "✈️"
+    tableRow.append(tableIcon)
+
+    const flightDetails = {
+        time: flight.departing.slice(0,5),
+        destination: flight.destination.toUpperCase(),
+        flight: flight.flightNumber.shift()
+        gate: flight.gate,
+        remarks: flight.status.toUpperCase()
+    }
+
+    for (const flightDetails in flightDetails) {
+        document.createElement('td')
+    }
+
+
+    tableBody.append(tableRow)
+    }
+}
